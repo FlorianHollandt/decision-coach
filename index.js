3fe14ccea6227fb590fc75d780beb77b2a816d8d
chombatant <florian.hollandt@gmail.com>
2017-10-04 07:26:50 +0200

    171004 Introduced states with respective handlers

--- index.js
+++ index.js
@@ -11,29 +11,54 @@ exports.handler = function( event, context, callback){
     alexa.appId = APP_ID;
     alexa.resources = languageStrings;
     alexa.registerHandlers( 
-        initializationHandler
+        newSessionHandlers,
+        quantifyOptionHandlers
         );
     alexa.execute();
 };
 
-var initializationHandler = {
+var states = {
+    QUANTIFY_OPTIONS: '_QUANTIFY_OPTIONS',
+    CONFIRM_TWO_OPTIONS: '_CONFIRM_TWO_OPTIONS',
+    CONFIRM_FOUR_OPTIONS: '_CONFIRM_FOUR_OPTIONS',
+    CONFIRM_OPTIONS : '_CONFIRM_OPTIONS'
+};
+
+var newSessionHandlers = {
     'LaunchRequest': function () {
-    console.log( "initializationHandler with LaunchRequest invoked...");
-    this.emit( ':ask', this.t( 'WELCOME') );
-    },    
+        console.log( "newSessionHandler with LaunchRequest invoked...");
+        this.handler.state = states.QUANTIFY_OPTIONS;
+        this.emit( ':ask', this.t( 'WELCOME') );
+    },
+    'Unhandled': function() {
+        console.log( "newSessionHandler with Unhandled Request invoked...");        
+        this.handler.state = states.QUANTIFY_OPTIONS;
+        this.emit( ':ask', this.t( 'WELCOME') );
+    }    
+};
+
+var quantifyOptionHandlers = Alexa.CreateStateHandler( states.QUANTIFY_OPTIONS, {
     'QuantityIntent': function () {
-        console.log( "initializationHandler with QuantityIntent invoked...");
+        console.log( "quantifyOptionHandlers with QuantityIntent invoked...");
         var number_of_options = get_number_from_quantity_slot( this.event.request.intent.slots.quantity.value);
         if ( number_of_options == 1){
             this.emit( ':ask', this.t( 'QUANTITY_ONE') );
         } else if ( number_of_options == 2){
             this.emit( ':ask', this.t( 'QUANTITY_TWO') );
+            this.handler.state = states.CONFIRM_TWO_OPTIONS;
+        } else if ( number_of_options > 4){
+            this.emit( ':ask', this.t( 'QUANTITY_TOO_MANY') );
+            this.handler.state = states.CONFIRM_FOUR_OPTIONS;            
         } else {
-            this.emit( ':tell', this.t( 'QUANTITY_GOOD') );
+            this.emit( ':ask', number_of_options + this.t( 'QUANTITY_GOOD') );
+            this.handler.state = states.CONFIRM_OPTIONS;                        
         }
-    }
-};
-
+    },
+    'Unhandled': function() {
+        console.log( "quantifyOptionHandlers with Unhandled Request invoked...");        
+        this.emit( ':ask', this.t( 'QUANTITY_REPEAT') );
+    }       
+});
 
 
 var languageStrings = {
@@ -42,27 +67,39 @@ var languageStrings = {
             'WELCOME'       : "Welcome to Decision Coach. I will gladly help you in making a \
                                difficult decision, by asking you a couple of questions. \
                                Let's start right away: Between how many options do you have to decide?",
-            'QUANTITY_GOOD' : " is a good amount of options.",
+            'QUANTITY_GOOD' : " is a good amount of options. Are you ready to answer some questions \
+                               about these options?",
             'QUANTITY_ONE'  : "It's not really a decision if there is only one option. \
-                              If you think about not pursuing the one option, or to do something \
-                              else entirely, how many realistic options do you have?",
+                               If you think about not pursuing the one option, or to do something \
+                               else entirely, how many realistic options do you have?",
             'QUANTITY_TWO'  : "Alright, two options are perfectly plausible. However, there is a \
-                              typical fallacy called the false dilemma, where people think they have \
-                              to decide between two options when actually there are more options \
-                              available. Are you sure there are only two options in your case?"                                
+                               typical fallacy called the false dilemma, where people think they have \
+                               to decide between two options when actually there are more options \
+                               available. Are you sure there are only two options in your case?",
+            'QUANTITY_TOO_MANY' : "Sorry, but these are too many options, if you want to \
+                               give a fair consideration for each of them. Can you narrow down your \
+                               range of options to the four most plausible ones?",
+            'QUANTITY_REPEAT' : "How many options did you say you have?"                               
         }
     },
     'de-DE': {
         'translation': {
             'WELCOME'       : "Willkommen beim Entscheidungs Coach. Zwischen wie vielen Optionen \
                                musst Du Dich entscheiden?",
-            'QUANTITY_GOOD' : " ist eine gute Anzahl von Optionen.",
+            'QUANTITY_GOOD' : " ist eine gute Anzahl von Optionen. Bist Du bereit, einige Fragen zu diesen \
+                               Optionen zu beantworten?",
             'QUANTITY_ONE'  : "Bei einer Option ist es nicht wirklich eine Entscheidung. Wenn Du \
                                auch die Möglichkeit einbeziehst, diese eine Option nicht umzusetzen, oder \
                                etwas ganz anderes zu tun, wie viele Möglichkeiten hast Du dann?",
             'QUANTITY_TWO'  : "Zwei Optionen sind vollkommen plausibel. Allerdings gibt es den häufigen \
                                Fehlschluss, es gäbe nur zwei Optionen, wenn es in Wirklichkeit noch \
-                               andere gute Möglichkeiten gibt. Bist Du sicher, dass Du nur zwei Optionen hast?"
+                               andere gute Möglichkeiten gibt. Bist Du sicher, dass Du nur zwei Optionen hast?",
+            'QUANTITY_TOO_MANY' : "Es tut mir leid, aber das sind zu viele Optionen, wenn Du eine faire \
+                               Abwägung vornehmen willst. Kannst Du Deine Optionen auf die vier plausibelsten \
+                               eingrenzen?",     
+            'QUANTITY_REPEAT' : "Kannst Du bitte nochmal wiederholen, zwischen wie vielen Optionen Du \
+                               Dich entscheiden musst?"                               
+
         }
     }    
 };
