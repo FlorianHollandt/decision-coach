060b1d6b4100a4995f9b291afd30c3ef6135a174
chombatant <florian.hollandt@gmail.com>
2017-10-06 09:34:10 +0200

    171006 Introduced handler for choiceIntent

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ exports.handler = function( event, context, callback){
     alexa.registerHandlers( 
         newSessionHandlers,
         quantifyOptionHandlers,
-        confirmOptionHandlers
+        confirmOptionHandlers,
+        polarQuestionHandlers,
+        choiceQuestionHandlers
         );
     alexa.execute();
 };
@@ -26,8 +28,7 @@ var states = {
     QUANTIFY_OPTIONS: '_QUANTIFY_OPTIONS',
     CONFIRM_OPTIONS : '_CONFIRM_OPTIONS',
     QUESTION_POLAR : '_QUESTION_POLAR',
-    QUESTION_CHOICE : '_QUESTION_CHOICE',
-    QUESTION_RELEVANCE : '_QUESTION_RELEVANCE' 
+    QUESTION_CHOICE : '_QUESTION_CHOICE'
 };
 
 //==================================================================================================
@@ -47,55 +48,160 @@ var newSessionHandlers = {
     }    
 };
 
+// --------------- quantifyOptionHandlers  -----------------------
+
 var quantifyOptionHandlers = Alexa.CreateStateHandler( states.QUANTIFY_OPTIONS, {
     'QuantityIntent': function () {
         console.log( "quantifyOptionHandlers with QuantityIntent invoked...");
-        var number_of_options = get_number_from_quantity_slot( this.event.request.intent.slots.quantity.value);
-        if ( number_of_options == 1){
+        this.attributes.number_of_options = get_number_from_slot( 
+            this.event.request.intent.slots.quantity.value
+        );
+        if ( this.attributes.number_of_options == 1){
             this.emit( ':ask', this.t( 'QUANTITY_ONE') );
-        } else if ( number_of_options == 2){
+        } else if ( this.attributes.number_of_options == 2){
             this.handler.state = states.CONFIRM_OPTIONS;
             this.emit( ':ask', this.t( 'QUANTITY_TWO') );
-        } else if ( number_of_options > 4){
+        } else if ( this.attributes.number_of_options > 4){
             this.handler.state = states.CONFIRM_OPTIONS;            
             this.emit( ':ask', this.t( 'QUANTITY_TOO_MANY') );
         } else {
             this.handler.state = states.CONFIRM_OPTIONS;                        
-            this.emit( ':ask', number_of_options + this.t( 'QUANTITY_GOOD') );
+            this.emit( ':ask', this.attributes.number_of_options + this.t( 'QUANTITY_GOOD') );
         }
     },
     'Unhandled': function() {
         console.log( "quantifyOptionHandlers with Unhandled Request invoked...");        
-        this.emit( ':ask', this.t( 'QUANTITY_REPEAT') );
+        this.emit( ':ask', this.t( 'REPEAT_QUANTITY') );
     }       
 });
 
+// --------------- confirmOptionHandlers  -----------------------
+
 var confirmOptionHandlers = Alexa.CreateStateHandler( states.CONFIRM_OPTIONS, {
     'YesIntent': function () {
-        console.log("confirmOptionHandlers with AMAZON.YesIntent invoked...");
+        console.log("confirmOptionHandlers with YesIntent invoked...");
         this.attributes.node_current = 'experience';
+        this.attributes.node_next = question_mapping[ this.attributes.node_current].next;;
         this.handler.state = question_mapping[ this.attributes.node_current].mode;
         this.emit( ':ask', this.t( question_mapping[ this.attributes.node_current].question) );
     },
     'QuantityIntent': function () {
         console.log( "confirmOptionHandlers with QuantityIntent invoked...");
-        var number_of_options = get_number_from_quantity_slot( this.event.request.intent.slots.quantity.value);
-        if ( number_of_options == 1){
+        this.attributes.number_of_options = get_number_from_slot( 
+            this.event.request.intent.slots.quantity.value
+        );
+        if ( this.attributes.number_of_options == 1){
             this.emit( ':ask', this.t( 'QUANTITY_ONE') );
-        } else if ( number_of_options == 2){
+        } else if ( this.attributes.number_of_options == 2){
             this.handler.state = states.CONFIRM_OPTIONS;
             this.emit( ':ask', this.t( 'QUANTITY_TWO') );
-        } else if ( number_of_options > 4){
+        } else if ( this.attributes.number_of_options > 4){
             this.handler.state = states.CONFIRM_OPTIONS;            
             this.emit( ':ask', this.t( 'QUANTITY_TOO_MANY') );
         } else {
             this.handler.state = states.CONFIRM_OPTIONS;                        
-            this.emit( ':ask', number_of_options + this.t( 'QUANTITY_GOOD') );
+            this.emit( ':ask', this.attributes.number_of_options + this.t( 'QUANTITY_GOOD') );
         }
     },
     'Unhandled': function() {
         console.log( "quantifyOptionHandlers with Unhandled Request invoked...");        
-        this.emit( ':ask', this.t( 'QUANTITY_REPEAT_2') );
+        this.emit( ':ask', this.t( 'REPEAT_QUANTITY_2') );
+    }       
+});
+
+// --------------- polarQuestionHandlers  -----------------------
+
+var polarQuestionHandlers = Alexa.CreateStateHandler( states.QUESTION_POLAR, {
+    'YesIntent': function () {
+        console.log("polarQuestionHandlers with YesIntent invoked...");
+        this.attributes[ this.attributes.node_current] = 1
+        // Start processing the next question
+        this.attributes.node_current = this.attributes.node_next;
+        this.attributes.node_next = question_mapping[ this.attributes.node_current].next;;
+        this.handler.state = question_mapping[ this.attributes.node_current].mode;
+        this.emit( ':ask', this.t( question_mapping[ this.attributes.node_current].question) );
+    },
+    'NoIntent': function () {
+        console.log("polarQuestionHandlers with NoIntent invoked...");
+        this.attributes[ this.attributes.node_current] = 0
+        // Start processing the next question
+        this.attributes.node_current = this.attributes.node_next;
+        this.attributes.node_next = question_mapping[ this.attributes.node_current].next;;
+        this.handler.state = question_mapping[ this.attributes.node_current].mode;
+        this.emit( ':ask', this.t( question_mapping[ this.attributes.node_current].question) );
+    },    
+    'Unhandled': function() {
+        console.log( "quantifyOptionHandlers with Unhandled Request invoked...");        
+        this.emit( ':ask', this.t( 'REPEAT_POLAR'));
+    }       
+});
+
+// --------------- choiceQuestionHandlers  -----------------------
+
+var choiceQuestionHandlers = Alexa.CreateStateHandler( states.QUESTION_CHOICE, {
+    'ChoiceIntent': function () {
+        console.log("choiceQuestionHandlers with ChoiceIntent invoked...");
+        var chosen_option = get_number_from_slot( 
+            this.event.request.intent.slots.choice.value
+        );
+        if ( chosen_option > this.attributes.number_of_options ){
+            console.log( 
+                "Invalid option chosen: " + chosen_option + 
+                ' of ' + this.attributes.number_of_options
+            );        
+            var text = this.t( 'REPEAT_CHOICE_TOO_HIGH').replace(
+                "XYZ",
+                this.attributes.number_of_options
+            )
+            this.emit( ':ask', text);
+        } else if ( chosen_option <= this.attributes.number_of_options &&
+            chosen_option > 0 ){
+            this.attributes[ this.attributes.node_current] = chosen_option;
+            // Start processing the next question
+            this.attributes.node_current = this.attributes.node_next;
+            this.attributes.node_next = question_mapping[ this.attributes.node_current].next;;
+            this.handler.state = question_mapping[ this.attributes.node_current].mode;
+            this.emit( ':ask', this.t( question_mapping[ this.attributes.node_current].question) );            
+        } else {
+            console.log( 
+                "Invalid option chosen: " + chosen_option
+            );
+            this.emit( ':ask', this.t( 'REPEAT_CHOICE')); // Better: 'REPEAT_CHOICE_3'
+        }
+    }, 
+    'QuantityIntent': function () {
+        console.log("choiceQuestionHandlers with QuantityIntent invoked...");
+        var chosen_option = get_number_from_slot( 
+            this.event.request.intent.slots.quantity.value
+        );
+        if ( chosen_option > this.attributes.number_of_options ){
+            console.log( 
+                "Invalid option chosen: " + chosen_option + 
+                ' of ' + this.attributes.number_of_options
+            );
+            var text = this.t( 'REPEAT_CHOICE_TOO_HIGH').replace(
+                "XYZ",
+                this.attributes.number_of_options
+            )
+            this.emit( ':ask', text);
+        } else if ( chosen_option <= this.attributes.number_of_options &&
+            chosen_option > 0 ){
+            this.attributes[ this.attributes.node_current] = chosen_option;
+            // Start processing the next question
+            this.attributes.node_current = this.attributes.node_next;
+            this.attributes.node_next = question_mapping[ this.attributes.node_current].next;;
+            this.handler.state = question_mapping[ this.attributes.node_current].mode;
+            this.emit( ':ask', this.t( question_mapping[ this.attributes.node_current].question) );            
+        } else {
+            console.log( 
+                "Invalid option chosen: " + chosen_option
+            );
+            this.emit( ':ask', this.t( 'REPEAT_CHOICE')); // Better: 'REPEAT_CHOICE_3'
+        }
+    },     
+    'Unhandled': function() {
+        console.log( "choiceQuestionHandlers with Unhandled Request invoked...");        
+        this.emit( ':ask', this.t( 'REPEAT_CHOICE')); // Better: 'REPEAT_CHOICE_3'
     }       
 });
 
@@ -113,7 +219,12 @@ var question_mapping = {
         'mode' : states.QUESTION_CHOICE,
         'question' : 'FEELING',
         'next' : 'relieve'        
-    }
+    },
+    'relieve' : {
+        'mode' : states.QUESTION_CHOICE,
+        'question' : 'RELIEVE',
+        'next' : 'longterm'        
+    }    
 }
 
 //==================================================================================================
@@ -138,15 +249,22 @@ var languageStrings = {
             'QUANTITY_TOO_MANY' : "Sorry, but these are too many options, if you want to \
                                give a fair consideration for each of them. Can you narrow down your \
                                range of options to the four most plausible ones?",
-            'QUANTITY_REPEAT' : "How many options did you say you have?",
-            'QUANTITY_REPEAT_2' : "In this case, how many relevant options do you have?",
-            'EXPERIENCE'    : "Do you have experience in making decisions like this one?"                           
+            'REPEAT_QUANTITY' : "How many options did you say you have?",
+            'REPEAT_QUANTITY_2' : "In this case, how many relevant options do you have?",
+            'REPEAT_POLAR'  : "Was this a yes or a no?",
+            'REPEAT_CHOICE' : "Please choose one option by saying 'Number one', 'option B', \
+                               'the third' or similar.",
+            'REPEAT_CHOICE_TOO_HIGH' : "You have narrowed down your options to XYZ before. \
+                               Please choose one of those, or start over again.",
+            'EXPERIENCE'    : "Do you have experience in making decisions like this one?",
+            'FEELING'       : "Which option feels most 'right' to you?",
+            'RELIEVE'       : "Which option would you feel most relieved about?"
         }
     },
     'de-DE': {
         'translation': {
-            'WELCOME'       : "Willkommen beim Entscheidungs Coach. Zwischen wie vielen Optionen \
-                               musst Du Dich entscheiden?",
+            'WELCOME'       : "Willkommen beim Entscheidungs Coach. Zwischen wie vielen Optionen " +
+                              " musst Du Dich entscheiden?",
             'QUANTITY_GOOD' : " ist eine gute Anzahl von Optionen. Bist Du bereit, einige Fragen zu diesen \
                                Optionen zu beantworten?",
             'QUANTITY_ONE'  : "Bei einer Option ist es nicht wirklich eine Entscheidung. Wenn Du \
@@ -158,10 +276,17 @@ var languageStrings = {
             'QUANTITY_TOO_MANY' : "Es tut mir leid, aber das sind zu viele Optionen, wenn Du eine faire \
                                Abwägung vornehmen willst. Kannst Du Deine Optionen auf die vier plausibelsten \
                                eingrenzen?",     
-            'QUANTITY_REPEAT' : "Kannst Du bitte nochmal wiederholen, zwischen wie vielen Optionen Du \
+            'REPEAT_QUANTITY' : "Kannst Du bitte nochmal wiederholen, zwischen wie vielen Optionen Du \
                                Dich entscheiden musst?",     
-            'QUANTITY_REPEAT_2' : "Zwischen wie vielen Optionen musst Du Dich in diesem Fall entscheiden?",
-            'EXPERIENCE'    : "Hast Du Erfahrung darin, Entscheidungen wie diese zu treffen?"
+            'REPEAT_QUANTITY_2' : "Zwischen wie vielen Optionen musst Du Dich in diesem Fall entscheiden?",
+            'REPEAT_POLAR'  : "War das ein Ja oder ein Nein?" ,
+            'REPEAT_CHOICE' : "Bitte wähle eine Option aus, indem Du 'Nummer Eins', 'Option B', \
+                               'die Dritte' or etwas ähnliches sagst.",
+            'REPEAT_CHOICE_TOO_HIGH' : "Du hattest Dich auf XYZ Optionen eingeschränkt. \
+                               Bitte wähle eine dieser Optionen aus, oder starte von vorne.",  
+            'EXPERIENCE'    : "Hast Du Erfahrung darin, Entscheidungen wie diese zu treffen?",
+            'FEELING'       : "Welche Option fühlt sich für Dich am meisten 'richtig' an?",
+            'RELIEVE'       : "Über welche Option wärst Du am meisten erleichtert?"            
         }
     }    
 };
@@ -170,37 +295,63 @@ var languageStrings = {
 // HELPERS
 //==================================================================================================
 
-function get_number_from_quantity_slot( key){
+function get_number_from_slot( key){
     var number_dict = {
         '1' : 1,
         1 : 1,
+        '1st' : 1,
+        'a' : 1,
         'eins' : 1,
         'eine' : 1,
         'eine einzige' : 1,
+        'erste' : 1,
         'one' : 1,
         'a single' : 1,
         'one single': 1,
         'only one' : 1,
+        'first' : 1,
         '2' : 2,
         2 : 2,
+        '2nd' : 2,
+        'b' : 2,
         'two' : 2,
+        'second': 2,
+        'other' : 2,
         'zwei' : 2,
+        'zweite' : 2,
+        'andere' : 2,
         '3' : 3,
         3 : 3,
+        '3rd' : 3,
+        'c' : 3,
         'three' : 3,
+        'third' : 3,
         'drei' : 3,
+        'dritte' : 3,
         4 : 4,
         '4' : 4,
+        '4th' : 4,
+        'd' : 4,
         'four' : 4,
+        'fourth' : 4,
         'vier' : 4,
+        'vierte' : 4,
         5 : 5,
         '5' : 5,
+        '5th' : 5,
+        'e' : 5,
         'five' : 5,
+        'fifth' : 5,
         'fünf' : 5,
+        'fünfte' : 5,
         6 : 6,
         '6' : 6,
+        '6th' : 6,
+        'f' : 6,
         'six' : 6,
-        'sechs' : 6
+        'sixth' : 6,
+        'sechs' : 6,
+        'sechste' : 6
     };
     return number_dict[ key];
 };
